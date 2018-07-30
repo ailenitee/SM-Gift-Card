13d6f3bf7a42e01bbc153b021547f397799e5ee0
Aileni <aileni.tee@glyphgames.com>
2018-07-30 13:12:36 +0800

    themes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 //    el: '#app'
 //});
 $(function() {
+  var referrer =  document.referrer;
   $('.signup-content-box').css('display','none');
   $('#detailModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
@@ -56,6 +57,8 @@ $(function() {
     $('.signup-content-box').css('display','none');
     $('.signup-content-box').css('opacity','0');
   });
+
+  //////// add border bottom when link is in active
   if(window.location.href.indexOf("login") > -1) {
     $('.nav-link').removeClass('active');
     $('.login').addClass('active');
@@ -73,16 +76,19 @@ $(function() {
   }else if(window.location.href.indexOf("card") > -1){
     $('.nav-link').removeClass('active');
     $('.details').addClass('btn-red');
-  }else{
-    $('.nav-link').removeClass('active');
+  }
+  var is_root = location.pathname == "/";
+  if(is_root){
+    // $('.nav-link').removeClass('active');
     $('.home').addClass('active');
   }
+
+  //////// show more and show less for design card
   $('.show-more').css('cursor','pointer');
   $('.show-less').css('cursor','pointer');
   var size_li = $('div.themes').length;
   var x=4;
   var last = size_li-x;
-  console.log(last);
   $('.hidden_input').val(8);
   $('.themes:lt(13)').hide();
   $('.themes:lt(-'+last+')').show();
@@ -116,5 +122,62 @@ $(function() {
     $('.themes:lt(-'+length+')').show();
     $('.show-less').css('display','none');
     $('.show-more').css('display','block');
+  });
+  ////////design choose between standard or upload own photo
+  $('.own').css('display','none');
+  $('.own').css('opacity','0');
+  $('#standard_btn').click(function () {
+    $('#photo_btn').removeClass('active');
+    $('#standard_btn').addClass('active');
+    $('.standard').css('display','block');
+    $('.standard').css('opacity','1');
+    $('.own').css('display','none');
+    $('.own').css('opacity','0');
+  });
+  $('#photo_btn').click(function () {
+    $('#standard_btn').removeClass('active');
+    $('#photo_btn').addClass('active');
+    $('.own').css('display','block');
+    $('.own').css('opacity','1');
+    $('.standard').css('display','none');
+    $('.standard').css('opacity','0');
+  });
+
+  ////////// on click theme change preview
+  $('.themes img').click(function () {
+    var imgsrc = $(this).attr('src');
+    $('#img-upload').attr('src',imgsrc);
+  });
+  ////////// upload photo with preview
+  $(document).on('change', '.btn-file :file', function() {
+      var input = $(this),
+          label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+      input.trigger('fileselect', [label]);
+  });
+  $('.btn-file :file').on('fileselect', function(event, label) {
+
+      var input = $(this).parents('.input-group').find(':text'),
+          log = label;
+
+      if( input.length ) {
+          input.val(log);
+      } else {
+          if( log ) alert(log);
+      }
+  });
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#img-upload').attr('src', e.target.result);
+              $('#img-upload').css('display','block');
+          }
+
+          reader.readAsDataURL(input.files[0]);
+      }
+  }
+  $("#imgInp").change(function(){
+      readURL(this);
   });
 });
--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -3,8 +3,19 @@
     <h2>Design</h2>
   </div>
   <div class="col-md-6">
-      <img src="http://via.placeholder.com/350x150" style='width:100%'>
-      <h4>Choose a theme</h4> 
+    <img src="{{URL::asset('/img/themes/bday-1.jpg')}}" id='img-upload' style='width:100%'>
+    <h4>Choose a theme</h4>
+    <div class="choose_btn">
+      <div class="row">
+        <div class="col-md-6">
+          <button type="button" class="btn btn-outline-primary active" id="standard_btn">Standard</button>
+        </div>
+        <div class="col-md-6">
+          <button type="button" class="btn btn-outline-primary" id="photo_btn">Your Photo</button>
+        </div>
+      </div>
+    </div>
+    <div class="standard">
       <div class="col-xs-3 col-md-3 themes">
         <img src="{{URL::asset('/img/themes/bday-1.jpg')}}" alt="">
       </div>
@@ -54,5 +65,22 @@
         </div>
       </div>
     </div>
+    <div class="own">
+      <div class="col-md-12">
+        <div class="form-group">
+          <label>Upload Image</label>
+          <br>
+          <div class="input-group upload-file">
+            <input type="text" class="form-control" readonly>
+            <span class="input-group-btn">
+              <span class="btn btn-default btn-file">
+                <input type="file" id="imgInp">
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="col-xs-3 col-md-3"></div>
 </div>
